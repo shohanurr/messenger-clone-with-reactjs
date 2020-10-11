4a828c59d2c414a225881b4ee12356d3120c1073
shohanurr <sohanbaf@gmail.com>
2020-10-11 18:14:33 +0600

    deleted all unused react file and installed material UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['hello', 'i love esha', 'i want to fuck her hard', 'want to suck her boobs,grab her ass']);
+
+  console.log(input);
+  console.log(messages);
+
+  //send message method
+  const sendMessage = event => {
+    event.preventDefault();
+    setMessages([...messages, input])
+    setInput('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello clever programmers 🚀 !</h1>
+
+      <form>
+        <input  value={input} onChange= {event => setInput(event.target.value)}/>
+        <button type='submit' onClick={sendMessage}> Send Message </button>
+      </form>
+   
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
