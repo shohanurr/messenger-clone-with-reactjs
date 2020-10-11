df8d660843f64f7112db4a1c63718d4ec92a5647
shohanurr <sohanbaf@gmail.com>
2020-10-11 22:27:41 +0600

    Adding firebase config

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+        apiKey: "AIzaSyCl6rARAjclcQ3A14qx3c59rf1IY7fj4YU",
+        authDomain: "messenger-clone-f1eea.firebaseapp.com",
+        databaseURL: "https://messenger-clone-f1eea.firebaseio.com",
+        projectId: "messenger-clone-f1eea",
+        storageBucket: "messenger-clone-f1eea.appspot.com",
+        messagingSenderId: "470603961198",
+        appId: "1:470603961198:web:0c12d291f0e9a6a741a77f"
+     
+});
+
+const db = firebaseApp.firestore();
+export default db;
